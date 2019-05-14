1661477ab3c91bb82b0569ab1b096ed3871429fa
Lucas <lucaspinheiroifsul@gmail.com>
2019-05-13 21:24:12 -0300

    comenta o cod que mantem o app acordado

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const app = require('./config/custom-express');
 const request = require('request');
 
+/*
 setInterval(function () {
     request('https://api-pratopratico.herokuapp.com/', function (error, response, body) {
         if (!error && response.statusCode == 200) {
@@ -9,5 +10,6 @@ setInterval(function () {
         }
     });
 }, 1500000); // sends a GET request every 25 minutes (1500000) to maintain the app awake
+*/
 
 app.listen(process.env.PORT || 5000);
